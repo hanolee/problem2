ca853b2a4a9d9db3f57f1dc62530ed7d73fe90b6
hanolee <hano.dev.kr@gmail.com>
2021-11-16 16:18:02 +0900

    completed

--- src/components/DynamicComponents.js
+++ src/components/DynamicComponents.js
@@ -1,22 +1,25 @@
-import { limitOfTx, setOfTx } from '../problem1';
+import { num, limitOfTx, setOfTx } from '../problem1';
 
 function DynamicComponents(props){
 
-    var newarr = new Array();
+    var newarr = new Array(num);
     
     for(var i=0;i<props.setOfTx.length;i++){
-        newarr += setOfTx[i];
+        
+        for(var j=0;j<props.setOfTx[i].length;j++){
+            newarr[i*10 + j] = setOfTx[i][j];
+        }
+        
     }
 
+
+    console.log("the type of newarr is ",typeof(newarr));
     console.log(newarr);
-    const arr = [1,2,3,4];
 
     return(
-        <div>
-            {newarr.map((num) => <div>{num}</div>)}
-        </div>
-        
-    );
+        newarr.map((num) => <div>{num}</div>)
+    );     
 }
+    
 
 export default DynamicComponents;
